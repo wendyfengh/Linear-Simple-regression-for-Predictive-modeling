cec419d48b6e71db7d0d55dc55343ded2c514e25
wendyfengh <wendyfengh@gmail.com>
2019-02-27 13:05:03 -0500

    from r studio

--- LInear regression with a database- Tree.R
+++ LInear regression with a database- Tree.R
@@ -8,8 +8,6 @@ head(trees)
 ggpairs(trees,columns=1:3,title='Three Database')
 ggpairs(trees,title='Tree database',mapping = aes(color = 'Relation'))
 
-## Identiying the relationship between a response variable or interest and one or more predictor(Cause and effect)
-
 reg = lm(Volume ~ Girth,data=trees)
 summary(reg)
 
@@ -29,7 +27,6 @@ ggplot(data = trees, aes(x = Girth, y = Volume)) + geom_point()  +
   ggtitle("Linear Regression Model Fitted to Data")
 
 
-##Prediction
 
 predict(reg,data.frame(Girth =18.2))
 
